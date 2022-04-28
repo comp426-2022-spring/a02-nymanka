5dc45ff0ffa528afe33beabfc3d69e590d8cb86a
Kaan Nyman <nymanka@ad.unc.edu>
2022-04-27 21:03:47 -0400

    y

--- guess-flip.js
+++ guess-flip.js
@@ -2,7 +2,7 @@ import {coinFlip, coinFlips, countFlips, flipACoin} from "./modules/coin.mjs"
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2))
-args[""]//call
+args[""]//call 
 const guess = args.call
 const info = flipACoin(guess);
 console.log(info)
