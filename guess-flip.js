177a3754e20c99db7b44442b8e6e35ff0f63b79f
Kaan Nyman <nymanka@ad.unc.edu>
2022-04-27 21:08:19 -0400

    mmn

--- guess-flip.js
+++ guess-flip.js
@@ -3,7 +3,7 @@ import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2))
 args[""]//call                
-
+ 
 const guess = args.call
 const info = flipACoin(guess);
 console.log(info)
